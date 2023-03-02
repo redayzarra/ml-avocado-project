42df4fbc01ba152c569c485febe3f1a2d3d3441d
ReDay Zarra <redayzarra@gmail.com>
2023-03-01 20:11:26 -0500

    Adding project description

---
+++ Avocado_Market.py
@@ -0,0 +1,12 @@
+"""
+Avocado Market - Time Series Regression
+
+This project utilizes Facebook Prophet to analyze time series data for 
+avocados from this dataset. The data includes observation dates, average 
+unit price, unit type, region, volume and more. This time series data will 
+then be used to predict the prices for avocados at any given date. This 
+project showcases a step-by-step implementation of the model as well as 
+in-depth notes to customize the model further for higher accuracy.
+
+We will first start by importing the necessary libraries we will need:
+"""
